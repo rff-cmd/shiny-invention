6f75e025d7128e0fe9ac9898d863d88cf5283ce4
rff-cmd <68042523+rff-cmd@users.noreply.github.com>
2022-06-15 12:27:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
    to: {
-     image: 'https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/288421700_3073152632902280_4446985111566740826_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGSoanW2w_ct2WkzENE7q4SXK4UBRRKN9hcrhQFFEo32FPIJiawJQlD2uOWo2W2h6VYleQpeVW_TjbV4WFUnzzs&_nc_ohc=vTskhMHAW-QAX9U6W9b&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT9a8Ie6PjjPsu5NuDuIpUkIfz4NL1030aKxrCEOOvjYhQ&oe=62ADECE1'
+     image: 'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos:6764b6ef800bc7f2209bbb83ce4fea2b20220614234603.png'
   },
 };
