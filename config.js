5b858cd2407be524eb391918dd2166848da8cabb
rff-cmd <68042523+rff-cmd@users.noreply.github.com>
2022-06-15 12:00:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
    to: {
-     image: 'https://www.facebook.com/photo/?fbid=3073152639568946&set=a.1514495808767978'
+     image: 'https://scontent.fbdo2-1.fna.fbcdn.net/v/t39.30808-6/288421700_3073152632902280_4446985111566740826_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGSoanW2w_ct2WkzENE7q4SXK4UBRRKN9hcrhQFFEo32FPIJiawJQlD2uOWo2W2h6VYleQpeVW_TjbV4WFUnzzs&_nc_ohc=vTskhMHAW-QAX9U6W9b&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT9a8Ie6PjjPsu5NuDuIpUkIfz4NL1030aKxrCEOOvjYhQ&oe=62ADECE1'
   },
 };
